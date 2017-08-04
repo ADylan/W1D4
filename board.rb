3908af585d9133734b8fdd2270e34b6f20f0ff27
App Academy Student <student@appacademy.io>
2017-08-03 17:23:37 -0700

    delete player.rb

--- board.rb
+++ board.rb
@@ -2,5 +2,12 @@ require_relative 'tile.rb'
 
 class Board
 
+  def initialize
+    @grid = Array.new(9) { Array.new(9) }
+    @grid.each do |arr|
+      arr.each {|space| space = Tile.new}
+    end
+  end
 
+  
 end
--- tile.rb
+++ tile.rb
@@ -1,5 +1,16 @@
 class Tile
+  attr_reader :face_down, :bomb
 
+  def initialize
+    @bomb = gen_bomb?
+    @face_down = true
+  end
 
-
+  def gen_bomb?
+    if rand(10) < 3
+      true
+    else
+      false
+    end
+  end
 end
